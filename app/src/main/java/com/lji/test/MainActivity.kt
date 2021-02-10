c629e4abc6a3ba338923340680efeba3a42b483a
AnkitDubeyBol <ankit.dubey@lji.io>
2021-02-10 20:30:44 +0530

    calculator screen added

--- app/src/main/java/com/lji/test/MainActivity.kt
+++ app/src/main/java/com/lji/test/MainActivity.kt
@@ -2,16 +2,22 @@ package com.lji.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var plusButton : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.portfolio_screen)
+        setContentView(R.layout.calculator_screen)
 
 
+        plusButton = findViewById(R.id.btn_add)
 
-
-
+        plusButton?.setOnClickListener {
+            println("Plus button clicked...")
+        }
 
     }
 }
